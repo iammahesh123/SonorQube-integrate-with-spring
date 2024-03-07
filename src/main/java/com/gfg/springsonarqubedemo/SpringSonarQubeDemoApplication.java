705b6f80eb3a52ebc954ecf4617d7763b914e59a
iammahesh123 <maheshkadambala18@gmail.com>
2024-03-07 13:52:45 +0530

    first commit

---
+++ src/main/java/com/gfg/springsonarqubedemo/SpringSonarQubeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gfg.springsonarqubedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSonarQubeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSonarQubeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gfg/springsonarqubedemo/SpringSonarQubeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfg.springsonarqubedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSonarQubeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
